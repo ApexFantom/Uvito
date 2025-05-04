7839162767faf45f5890e2cfe241d70f398584fb
Vadim Mayamsin <55667882+ApexFantom@users.noreply.github.com>
2025-05-04 08:41:11 +0300

    Update layers.js

--- src/main/resources/static/scripts/layers.js
+++ src/main/resources/static/scripts/layers.js
@@ -1,30 +1,26 @@
-function makeLayer(blank, display='flex', ...args) {
-  if (!document.getElementById(blank)) {
-    return 0;
-  }
-  var blankLayer = document.getElementById(blank);
-
-  for (let btn of args) {
-    const button = document.getElementById(btn);
-    if (button) {
-      button.onclick = () => {
-          if (blankLayer.style.display === 'none') {
-    blankLayer.style.display = display;
-    blankLayer.style.zIndex = 10;
-  } else {
-      document.getElementById('name').value = '';
-      document.getElementById('des').value = '';
-      document.getElementById('file-msg-icon').innerHTML = '';
-      document.getElementById('create-blank-h').innerHTML = 'Создать объявление';
-      document.getElementById('img_area').style.backgroundColor = '#00000000';
-      document.getElementById('item-name').numberId = '';
-      document.getElementById('CreateButton').style.display = 'block';
-      document.getElementById('UpdateButton').style.display = 'none';
-      document.getElementById('DeleteButton').style.display = 'none';
-    blankLayer.style.display = 'none';
-    blankLayer.style.zIndex = -999;
-  }
+for (let btn of args) {
+  const button = document.getElementById(btn);
+  if (button) {
+    const toggleLayer = () => {
+      if (blankLayer.style.display === 'none') {
+        blankLayer.style.display = display;
+        blankLayer.style.zIndex = 10;
+      } else {
+        document.getElementById('name').value = '';
+        document.getElementById('des').value = '';
+        document.getElementById('file-msg-icon').innerHTML = '';
+        document.getElementById('create-blank-h').innerHTML = 'Создать объявление';
+        document.getElementById('img_area').style.backgroundColor = '#00000000';
+        document.getElementById('item-name').numberId = '';
+        document.getElementById('CreateButton').style.display = 'block';
+        document.getElementById('UpdateButton').style.display = 'none';
+        document.getElementById('DeleteButton').style.display = 'none';
+        blankLayer.style.display = 'none';
+        blankLayer.style.zIndex = -999;
       }
-    }
+    };
+
+    button.addEventListener('click', toggleLayer);
+    button.addEventListener('touchstart', toggleLayer);
   }
 }
